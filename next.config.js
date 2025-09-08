ae631cd9f56a79986892a651b957548a72f7c48a
sanctity2k5 <sanctity2k5@gmail.com>
2025-09-08 22:00:18 +0100

    fix next config - two

--- next.config.js
+++ next.config.js
@@ -1,11 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export', // keep this ONLY if you want a fully static export
   eslint: {
     ignoreDuringBuilds: true,
   },
   images: {
-    unoptimized: true, // required for static export
+    unoptimized: true, 
   },
 };
 
