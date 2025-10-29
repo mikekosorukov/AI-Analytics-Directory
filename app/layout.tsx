1ba1acacbda364b14d9653d80bbe23d640969b39
MLG <prog@itdelta.agency>
2025-10-29 11:49:55 +0300

    feat(posthog): add lib

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,10 +2,11 @@ import './globals.css';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import { Toaster } from '@/components/ui/toaster';
+import PostHogProvider from './posthog-provider';
 
 const inter = Inter({ subsets: ['latin'] });
 
-export const metadata = {
+export const metadata: Metadata = {
 	title: 'AI Analytics Tools - Find the Right Tool for the Job',
 	description:
 		'Explore the emerging AI analytics landscape and discover the perfect tools for your data analysis needs.',
@@ -35,7 +36,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        {children}
+        <PostHogProvider>{children}</PostHogProvider>
         <Toaster />
       </body>
     </html>
