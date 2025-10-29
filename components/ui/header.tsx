515c56d13a55de348ab1be4455e6c8baf77d3403
MLG <prog@itdelta.agency>
2025-10-29 11:51:03 +0300

    fix(header): add change tab by click logo

--- components/ui/header.tsx
+++ components/ui/header.tsx
@@ -56,7 +56,7 @@ export default function Header() {
 			<div className='max-w-7xl mx-auto px-4 sm:px-6 lg:px-8'>
 				<div className='flex justify-between items-center h-16'>
 					{/* Logo */}
-					<Link href={'/'} className='flex items-center space-x-2'>
+					<Link href={'/'} className='flex items-center space-x-2' onClick={() => setActiveTab('explore')}>
 						<Image src={Logo} width='40' alt='logo' />
 						<span className='text-white drop-shadow-lg'>AI Analytics Hub</span>
 					</Link>
